8f603923ed891e0d8d0bd0d00ed17ce5b373cc1a
max <tool13@mail.ru>
2025-10-30 08:20:04 +0300

    add transitions

--- scripts.js
+++ scripts.js
@@ -5,3 +5,17 @@ let swiper = new Swiper(".reviews__slider", {
         prevEl: ".reviews__arrow--prev"
     },
 });
+
+document.querySelector(".header__nav-btn").addEventListener("click", evt=> {
+  evt.target.classList.toggle("active");
+  document.querySelector(".header__links").classList.toggle("active");
+})
+
+let headerFigure = document.querySelector(".header__pic path");
+let length = headerFigure.getTotalLength();
+
+headerFigure.style.strokeDasharray = length + ' ' + length;
+headerFigure.style.strokeDashoffset = length;
+headerFigure.getBoundingClientRect();
+headerFigure.style.transition = headerFigure.style.WebkitTransition = 'stroke-dashoffset 3s ease-in-out';
+headerFigure.style.strokeDashoffset = 0;
